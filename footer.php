1944c5e571ed2b20b71e7695365e589efb361db1
sadiejay <19538219+sadiejay@users.noreply.github.com>
2017-11-02 00:28:23 -0400

    Add footer html

--- footer.php
+++ footer.php
@@ -1,3 +1,68 @@
+</main>
+
+
+<!-- footer -->
+
+<footer>
+	<div class="logo footer__logo">
+		<img src="#">
+	</div><!-- logo  -->
+
+	<div class="footer__contact">
+		<ul>
+			<li class="mask fb--mask"><a href="https://www.facebook.com/ThatBGTLife/" target="_blank" rel="noopener"></a></li>
+			<li class="mask insta--mask"><a href="https://www.instagram.com/blackgirltravel/" target="_blank" rel="noopener"></a></li>
+			<li class="mask yt--mask"><a href="https://www.youtube.com/user/BlackGirlTravel" target="_blank" rel="noopener"></a></li>
+		</ul>
+	</div><!--  contact -->
+
+	<div class="footer__blog">
+		<div class="blog__individual1">
+
+			<div class="blog__thumb">
+				<img src="img/vonnie.jpg" alt="blog post photo">
+			</div><!-- trip thumbnail-->
+
+			<div class="blog__text">
+				<p>Greece 2018</p>
+			</div><!-- text-->
+
+			<button class="button button__read">read more</button>
+
+		</div><!--trip individual1-->
+
+		<div class="blog__individual2">
+
+			<div class="blog__thumb">
+				<img src="img/blog2.jpg" alt="blog post photo">
+			</div><!-- trip thumbnail-->
+
+			<div class="blog__text">
+				<p>Girls Gone Wild! South Africa 2018</p>
+			</div><!-- text-->
+
+			<button class="button button__read">read more</button>
+
+		</div><!--trip individual2-->
+
+		<div class="blog__individual3">
+
+			<div class="blog__thumb">
+				<img src="img/blog3.jpg" alt="blog post photo">
+			</div><!-- blog thumbnail-->
+
+			<div class="blog__text">
+				<p>Holibeach Party</p>
+			</div><!-- text-->
+
+			<button class="button button__read">read more</button>
+
+		</div><!--blog individual3-->
+
+	</div><!-- footer blog -->
+
+</footer>
+
 		<?php wp_footer(); ?>
 
 		<!-- TODO: ADD GOOGLE ANALYTICS -->
