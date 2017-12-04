d32698b26f6557fef626f94931c7942c6f7bd1dd
sadiejay <19538219+sadiejay@users.noreply.github.com>
2017-12-04 02:36:13 -0500

    Set up tag page

--- tag.php
+++ tag.php
@@ -1,19 +1,33 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<!-- blog -->
 
-			<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
+<section class="blog">
+	<h1>Blog Posts</h1>
+	<h2><?php _e( 'Tag: ', 'html5blank' ); echo single_tag_title('', false); ?></h2>
 
-			<?php get_template_part('partials/loop'); ?>
+<?php if(have_posts()): while(have_posts()): the_post();?>
 
-			<?php get_template_part('partials/pagination'); ?>
+	<article class=" blog__i blog__individual1">
 
-		</section>
-		<!-- /section -->
-	</main>
+		<div class="blog__thumb">
+			<?php the_post_thumbnail(); ?>
+		</div><!-- trip thumbnail-->
 
-<?php get_sidebar(); ?>
+		<div class="blog__date">
+			<?php echo get_the_date(); ?>
+		</div><!-- date-->
+
+		<div class="blog__text">
+			<?php the_title(); ?>
+		</div><!-- text-->
+
+		<button class="button button__read"><a href="<?php the_permalink(); ?>" target="_blank" rel="noopener">read more</a></button>
+
+	</article><!--trip individual1-->
+
+<?php endwhile; endif; ?>
+
+</section><!-- blog -->
 
 <?php get_footer(); ?>
