95efb35ad09829a8095a0a69a47674486303b24e
sadiejay <19538219+sadiejay@users.noreply.github.com>
2017-12-03 22:57:15 -0500

    Add loop for posts

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 <section class="blog">
 	<h2>Blog Posts</h2>
 
+<? ​php​​​ if​​​​ (​have_posts​())​​​​: ?>
+	​​​​<? php while​​​​ (​have_posts​())​​​​:​​​
+	the_post​();​​​​?>
+
 	<article class=" blog__i blog__individual1">
 
 		<div class="blog__thumb">
@@ -19,78 +23,9 @@
 
 	</article><!--trip individual1-->
 
-	<article class="blog__i blog__individual2">
+	<?​php​​ endwhile​;​​​ ?>
+<?php endif​;​​​​?>
 
-		<div class="blog__thumb">
-			<img src="img/blog2.jpg"
-			alt="standing in front of statue with italian carving">
-		</div><!-- trip thumbnail-->
-
-		<div class="blog__text">
-			<p>Girls Gone Wild! South Africa 2018</p>
-		</div><!-- text-->
-
-		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
-
-	</article><!--trip individual2-->
-
-	<article class="blog__i blog__individual3">
-
-		<div class="blog__thumb">
-			<img src="img/blog3.jpg" alt="posing in Bali">
-		</div><!-- blog thumbnail-->
-
-		<div class="blog__text">
-			<p>Holibeach Party</p>
-		</div><!-- text-->
-
-		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
-
-	</article><!--blog individual3-->
-
-
-	<article class=" blog__i blog__individual4">
-
-		<div class="blog__thumb">
-			<img src="img/vonnie.jpg" alt="Girl standing in front of a grafittied door">
-		</div><!-- trip thumbnail-->
-
-		<div class="blog__text">
-			<p>Greece 2018</p>
-		</div><!-- text-->
-
-		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
-
-	</article><!--trip individual4-->
-
-	<article class="blog__i blog__individual5">
-
-		<div class="blog__thumb">
-			<img src="img/blog2.jpg"
-			alt="standing in front of statue with italian carving">
-		</div><!-- trip thumbnail-->
-
-		<div class="blog__text">
-			<p>Girls Gone Wild! South Africa 2018</p>
-		</div><!-- text-->
-
-		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
-
-	</article><!--trip individual5-->
-
-	<article class="blog__i blog__individual6">
-
-		<div class="blog__thumb">
-			<img src="img/blog3.jpg" alt="posing in Bali">
-		</div><!-- blog thumbnail-->
-
-		<div class="blog__text">
-			<p>Holibeach Party</p>
-		</div><!-- text-->
-
-		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
-
-	</article><!--blog individual6-->
 
 </section><!-- blog -->
 
