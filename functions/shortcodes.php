7f8fdd382e881f12d4267cba4e72b9f57ef24d23
sadiejay <19538219+sadiejay@users.noreply.github.com>
2017-12-04 02:53:16 -0500

    Create a shortcode

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -1,6 +1,6 @@
 <?php
 // Shortcodes
-// add_shortcode('html5_shortcode_demo', 'html5_shortcode_demo'); // You can place [html5_shortcode_demo] in Pages, Posts now.
+add_shortcode('heading', 'heading_shortcode'); // You can place [html5_shortcode_demo] in Pages, Posts now.
 // add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2'); // Place [html5_shortcode_demo_2] in Pages, Posts now.
 
 // Shortcodes above would be nested like this -
@@ -11,15 +11,15 @@
 \*------------------------------------*/
 
 // Shortcode Demo with Nested Capability
-/*
+
 function html5_shortcode_demo($atts, $content = null)
 {
-    return '<div class="shortcode-demo">' . do_shortcode($content) . '</div>'; // do_shortcode allows for nested Shortcodes
+    return '<h3>' . do_shortcode($content) . '</h3>'; // do_shortcode allows for nested Shortcodes
 }
 
 // Shortcode Demo with simple <h2> tag
-function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 shortcode, allows for nesting within above element. Fully expandable.
-{
-    return '<h2>' . $content . '</h2>';
-}
-*/
+// function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 shortcode, allows for nesting within above element. Fully expandable.
+// {
+//    return '<h2>' . $content . '</h2>';
+// }
+// */
