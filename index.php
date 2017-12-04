1798720e27481e04438d8f13dea801addad9914e
sadiejay <19538219+sadiejay@users.noreply.github.com>
2017-12-03 22:02:43 -0500

    Hotwire blog to index.php

--- index.php
+++ index.php
@@ -1,19 +1,97 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<!-- blog -->
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+<section class="blog">
+	<h2>Blog Posts</h2>
 
-			<?php get_template_part('partials/loop'); ?>
+	<article class=" blog__i blog__individual1">
 
-			<?php get_template_part('partials/pagination'); ?>
+		<div class="blog__thumb">
+			<img src="img/vonnie.jpg" alt="Girl standing in front of a grafittied door">
+		</div><!-- trip thumbnail-->
 
-		</section>
-		<!-- /section -->
-	</main>
+		<div class="blog__text">
+			<p>Greece 2018</p>
+		</div><!-- text-->
 
-<?php get_sidebar(); ?>
+		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
+
+	</article><!--trip individual1-->
+
+	<article class="blog__i blog__individual2">
+
+		<div class="blog__thumb">
+			<img src="img/blog2.jpg"
+			alt="standing in front of statue with italian carving">
+		</div><!-- trip thumbnail-->
+
+		<div class="blog__text">
+			<p>Girls Gone Wild! South Africa 2018</p>
+		</div><!-- text-->
+
+		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
+
+	</article><!--trip individual2-->
+
+	<article class="blog__i blog__individual3">
+
+		<div class="blog__thumb">
+			<img src="img/blog3.jpg" alt="posing in Bali">
+		</div><!-- blog thumbnail-->
+
+		<div class="blog__text">
+			<p>Holibeach Party</p>
+		</div><!-- text-->
+
+		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
+
+	</article><!--blog individual3-->
+
+
+	<article class=" blog__i blog__individual4">
+
+		<div class="blog__thumb">
+			<img src="img/vonnie.jpg" alt="Girl standing in front of a grafittied door">
+		</div><!-- trip thumbnail-->
+
+		<div class="blog__text">
+			<p>Greece 2018</p>
+		</div><!-- text-->
+
+		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
+
+	</article><!--trip individual4-->
+
+	<article class="blog__i blog__individual5">
+
+		<div class="blog__thumb">
+			<img src="img/blog2.jpg"
+			alt="standing in front of statue with italian carving">
+		</div><!-- trip thumbnail-->
+
+		<div class="blog__text">
+			<p>Girls Gone Wild! South Africa 2018</p>
+		</div><!-- text-->
+
+		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
+
+	</article><!--trip individual5-->
+
+	<article class="blog__i blog__individual6">
+
+		<div class="blog__thumb">
+			<img src="img/blog3.jpg" alt="posing in Bali">
+		</div><!-- blog thumbnail-->
+
+		<div class="blog__text">
+			<p>Holibeach Party</p>
+		</div><!-- text-->
+
+		<button class="button button__read"><a href="blog-single.html" target="_blank" rel="noopener">read more</a></button>
+
+	</article><!--blog individual6-->
+
+</section><!-- blog -->
 
 <?php get_footer(); ?>
