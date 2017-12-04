66fa0d70dcb2867c1cdc3388739c8265885c8efa
sadiejay <19538219+sadiejay@users.noreply.github.com>
2017-12-04 09:17:02 -0500

    Create Tours custom post type

--- functions/custom-post-types.php
+++ functions/custom-post-types.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*------------------------------------*\
 	Custom Post Types
 \*------------------------------------*/
@@ -45,4 +45,42 @@ function create_post_type_html5()
         )
     );
     */
+
+    register_post_type('tour', // Register Custom Post Type
+        array(
+        'labels' => array(
+            'name' => __('Tours', 'html5blank'), // Rename these to suit
+            'singular_name' => __('Tour', 'html5blank'),
+            'add_new' => __('Add New', 'html5blank'),
+            'add_new_item' => __('Add New Tour', 'html5blank'),
+            'edit' => __('Edit', 'html5blank'),
+            'edit_item' => __('Edit Tour', 'html5blank'),
+            'new_item' => __('New Tour', 'html5blank'),
+            'view' => __('View Tours', 'html5blank'),
+            'view_item' => __('View Tour', 'html5blank'),
+            'search_items' => __('Search Tours', 'html5blank'),
+            'not_found' => __('No Tours found', 'html5blank'),
+            'not_found_in_trash' => __('No Tours found in Trash', 'html5blank')
+        ),
+        'public' => true,
+        'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
+        'has_archive' => true,
+        'supports' => array(
+            'title',
+            'excerpt',
+            'thumbnail'
+        ), // Go to Dashboard Custom HTML5 Blank post for supports
+        'menu_icon' => 'dashicons-location',   // http://www.kevinleary.net/wordpress-dashicons-list-custom-post-type-icons/
+        'can_export' => true // Allows export in Tools > Export
+    ));
+    register_taxonomy(
+        'type_of_tours',    // Taxonomy
+        'tours',             // Object Type
+        array(
+            'label' => __( 'Type of Tours' ),
+            'rewrite' => array( 'slug' => 'tours' ),
+            'hierarchical' => true, // Is this taxonomy hierarchical like categories or not hierarchical like tags.
+        )
+    );
+
 }
